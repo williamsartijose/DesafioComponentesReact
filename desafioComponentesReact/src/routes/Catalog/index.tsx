cfedf41be61ecb336a7fadefac86ce7d8e4d1d52
unknown <williamsartijose@hotmail.com>
2023-10-20 17:18:02 +0100

    Primeiro componente Header

---
+++ desafioComponentesReact/src/routes/Catalog/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css'
+import Header from '../../components/Header';
+
+
+
+export default function Catalog () {
+    return (
+        <>
+        <Header/>
+        <main>
+            <section id="ct-catalog-section">
+                <div className="ct-catalog-cards-container">
+
+                </div>
+
+
+                <div>
+
+
+                </div>
+            </section>
+        </main>
+        </>
+    );
+}
